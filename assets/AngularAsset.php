39090d07d6a0f962a6f2018abb978b4d85b8c250
Atre <mo0surf@gmail.com>
2015-09-01 08:41:51 +0300

    composer update

---
+++ assets/AngularAsset.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\assets;
+
+
+use yii\web\AssetBundle;
+use yii\web\View;
+
+class AngularAsset extends AssetBundle
+{
+    public $sourcePath = '@bower';
+    public $js = [
+        'jquery/dist/jquery.js',
+        'angular/angular.js',
+        'angular-route/angular-route.js',
+        'angular-strap/dist/angular-strap.js',
+        'angular-resource/angular-resource.js',
+        'bootstrap/dist/js/bootstrap.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.js',
+    ];
+    public $css = [
+        'bootstrap/dist/css/bootstrap.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/themes/red/pace-theme-minimal.css',
+    ];
+    public $jsOptions = [
+        'position' => View::POS_HEAD
+    ];
+}
